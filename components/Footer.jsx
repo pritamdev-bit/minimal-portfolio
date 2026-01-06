afb59eacbcecb52c9a7b06c4adc8628fe031a931
Pritam Mandal <mandalpritam8617@gmail.com>
2026-01-07 01:23:08 +0530

    Auto Year Update Feature

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Footer = () => {
+  const getYear = ()=> {
+    const newDate = new Date();
+    return newDate.getFullYear();
+  }
+  const currentYear = getYear();
+
   return (
     <div className='my-6'>
       <footer className='w-full text-center'>
         <p>Designed & Developed by <span className='font-bold'>Pritam Mandal</span></p>
-        <p>&copy; 2025. All rights reserved.</p>
+        <p>&copy; {currentYear}. All rights reserved.</p>
       </footer>
     </div>
   )
